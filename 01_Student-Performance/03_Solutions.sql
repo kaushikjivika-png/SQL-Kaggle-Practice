592d479f6c9c8a10301a0b1e99335bf8af18e04a
Jivika Kaushik <kaushikjivika@gmail.com>
2026-06-19 23:04:42 +0530

    solve Q1 to Q7.

--- 01_Student-Performance/03_Solutions.sql
+++ 01_Student-Performance/03_Solutions.sql
@@ -1,1 +1,25 @@
+Q1--
+SELECT COUNT(*)
+FROM student;
 
+Q2--
+SELECT * FROM student;
+
+Q3--
+SELECT name,age FROM student;
+
+Q4--
+SELECT * FROM student
+WHERE age > 18;
+
+Q5--
+SELECT * FROM student
+WHERE age < 18;
+
+Q6--
+SELECT * FROM student
+WHERE school = 'GP';
+
+Q7--
+SELECT * FROM student
+WHERE school = 'MS';
