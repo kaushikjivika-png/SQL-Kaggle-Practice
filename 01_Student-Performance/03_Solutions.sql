2a8a6b2493f599dc9883a3a4fdb1b518c729b81b
Jivika Kaushik <kaushikjivika@gmail.com>
2026-06-20 18:08:20 +0530

    solved Q8 to Q23.
    Uncomment SQL queries for student performance analysis.

--- 01_Student-Performance/03_Solutions.sql
+++ 01_Student-Performance/03_Solutions.sql
@@ -23,3 +23,70 @@ WHERE school = 'GP';
 Q7--
 SELECT * FROM student
 WHERE school = 'MS';
+
+Q8--
+SELECT * FROM student
+WHERE g3 > 15;
+
+Q9--
+SELECT * FROM student
+WHERE g3 < 10;
+
+Q10--
+SELECT * FROM student
+ORDER BY age;
+
+Q11--
+SELECT * FROM student
+ORDER BY g3 DESC;
+
+Q12--
+SELECT * FROM student
+ORDER BY g3 DESC
+LIMIT 5;
+
+Q13--
+SELECT * FROM student
+ORDER BY g3 
+LIMIT 5;
+
+Q14--
+SELECT sex, COUNT(*)
+FROM student
+GROUP BY sex;
+
+Q15--
+SELECT DISTINCT(school)
+FROM student;
+
+Q16--
+SELECT * FROM student
+WHERE sex = 'F';
+
+Q17--
+SELECT * FROM student
+WHERE sex = 'M';
+
+Q18--
+SELECT * FROM student
+WHERE age > 17;
+
+Q19--
+SELECT * FROM student
+WHERE failures > 0;
+
+Q20--
+SELECT * FROM student
+WHERE studytime = 4;
+
+Q21--
+SELECT * FROM student
+WHERE traveltime = 1;
+
+Q22--
+SELECT * FROM student
+WHERE internet = 'yes';
+
+Q23--
+SELECT * FROM student
+WHERE schoolsup = 'yes';
